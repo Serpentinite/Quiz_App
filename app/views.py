bca11cd34e8504f816a9b8aa21c376431e306114
Serpentinite <zhiyeg425@gmail.com>
2021-05-13 20:48:52 +0900

    9th Commit

--- app/views.py
+++ app/views.py
@@ -47,10 +47,10 @@ class QuizDeleteView(DeleteView):
 
 # エラーハンドリング
 def page_not_found(request, exception):
-    return render(request, 'quiz/404.html', status=500)
+    return render(request, '404.html', status=404)
 
 def server_error(request):
-    return render(request, 'quiz/500.html', status=500)
+    return render(request, '500.html', status=500)
 
 
 
