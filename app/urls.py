e1f874e5e9adb81d6296e651af577ac4fb595a44
Serpentinite <zhiyeg425@gmail.com>
2021-05-13 16:37:06 +0900

    First Commit

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    HomeView, QuizListView, QuizCreateView, QuizUpdateView, QuizDeleteView,
+)
+from . import views
+
+app_name = 'app'
+
+urlpatterns = [
+    path('',HomeView.as_view(), name='home'),
+    path('create_quiz/', QuizCreateView.as_view(), name='create_quiz'),
+    path('quiz_list/', QuizListView.as_view(), name='quiz_list'),
+    path('update_quiz/<int:pk>', QuizUpdateView.as_view(), name='update_quiz'),
+    path('delete_quiz/<int:pk>', QuizDeleteView.as_view(), name='delete_quiz'),
+]
---
+++ app/views.py
@@ -0,0 +1,77 @@
+from os import stat
+from django.forms.forms import Form
+from django.shortcuts import render, redirect
+from . import forms
+from django.contrib import messages
+from .models import Quiz
+from django.views.generic.base import (
+    TemplateView,
+)
+from django.views.generic.edit import (
+    CreateView, UpdateView, DeleteView, FormView,
+)
+from django.views.generic.list import ListView
+from pure_pagination.mixins import PaginationMixin
+from django.urls import reverse_lazy
+# from django.contrib.auth.mixins import 
+class HomeView(TemplateView):
+    template_name='home.html'
+
+class QuizCreateView(CreateView):
+    template_name = 'create_quiz.html'
+    form_class = forms.CreateQuizForm
+    model = Quiz
+
+    def get_initial(self, **kwargs):
+        initial = super(QuizCreateView, self).get_initial(**kwargs)
+        initial['category'] = 'その他'
+        return initial
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+
+
+class QuizListView(PaginationMixin, ListView):
+    model = Quiz
+    template_name = 'quiz_list.html'
+    paginate_by = 10
+
+class QuizUpdateView( UpdateView):
+    model = Quiz
+    template_name = 'update_quiz.html'
+    form_class = forms.QuizUpdateForm
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+
+
+class QuizDeleteView( DeleteView):
+    model = Quiz
+    template_name = 'delete_quiz.html'
+    success_url = reverse_lazy('quiz:quiz_list')
+     
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+class QuizFormView(ListView):
+    template_name = 'home.html'
+    model = Quiz
+    def get_queryset(self):
+        return Quiz.objects.order_by('?')[:1]
+
+# # エラーハンドリング
+# def page_not_found(request, exception):
+#     return render(request, 'quiz/404.html', status=500)
+
+# def server_error(request):
+#     return render(request, 'quiz/500.html', status=500)
+
+
+
+
+
+
+
+
